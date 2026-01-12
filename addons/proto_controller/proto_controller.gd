fecaaaad7fe926c11835697b19ca99c6f38ecbd5
MertFindik97 <82378706+MertFindik97@users.noreply.github.com>
2026-01-12 18:16:56 +0100

    dash und WASD

--- addons/proto_controller/proto_controller.gd
+++ addons/proto_controller/proto_controller.gd
@@ -1,7 +1,6 @@
 # ProtoController v1.0 by Brackeys
 # CC0 License
-# Intended for rapid prototyping of first-person games.
-# Happy prototyping!
+# Extended: WASD style input, sprint fix, dash added
 
 extends CharacterBody3D
 
@@ -12,7 +11,9 @@ extends CharacterBody3D
 ## Can we press to jump?
 @export var can_jump : bool = true
 ## Can we hold to run?
-@export var can_sprint : bool = false
+@export var can_sprint : bool = true
+## Can we dash?
+@export var can_dash : bool = true
 ## Can we press to enter freefly mode (noclip)?
 @export var can_freefly : bool = false
 
@@ -28,19 +29,28 @@ extends CharacterBody3D
 ## How fast do we freefly?
 @export var freefly_speed : float = 25.0
 
+@export_group("Dash")
+@export var dash_speed : float = 18.0
+@export var dash_duration : float = 0.15
+@export var dash_cooldown : float = 0.6
+## Optional: only allow dash on floor
+@export var dash_only_on_floor : bool = false
+
 @export_group("Input Actions")
 ## Name of Input Action to move Left.
-@export var input_left : String = "ui_left"
+@export var input_left : String = "move_left"
 ## Name of Input Action to move Right.
-@export var input_right : String = "ui_right"
+@export var input_right : String = "move_right"
 ## Name of Input Action to move Forward.
-@export var input_forward : String = "ui_up"
+@export var input_forward : String = "move_forward"
 ## Name of Input Action to move Backward.
-@export var input_back : String = "ui_down"
+@export var input_back : String = "move_back"
 ## Name of Input Action to Jump.
-@export var input_jump : String = "ui_accept"
+@export var input_jump : String = "jump"
 ## Name of Input Action to Sprint.
 @export var input_sprint : String = "sprint"
+## Name of Input Action to Dash.
+@export var input_dash : String = "dash"
 ## Name of Input Action to toggle freefly mode.
 @export var input_freefly : String = "freefly"
 
@@ -48,6 +58,12 @@ var mouse_captured : bool = false
 var look_rotation : Vector2
 var move_speed : float = 0.0
 var freeflying : bool = false
+
+# Dash state
+var dashing : bool = false
+var dash_time_left : float = 0.0
+var dash_cd_left : float = 0.0
+var dash_dir : Vector3 = Vector3.ZERO
 
 ## IMPORTANT REFERENCES
 @onready var head: Node3D = $Head
@@ -64,11 +80,11 @@ func _unhandled_input(event: InputEvent) -> void:
 		capture_mouse()
 	if Input.is_key_pressed(KEY_ESCAPE):
 		release_mouse()
-	
+
 	# Look around
 	if mouse_captured and event is InputEventMouseMotion:
 		rotate_look(event.relative)
-	
+
 	# Toggle freefly mode
 	if can_freefly and Input.is_action_just_pressed(input_freefly):
 		if not freeflying:
@@ -84,8 +100,12 @@ func _physics_process(delta: float) -> void:
 		motion *= freefly_speed * delta
 		move_and_collide(motion)
 		return
-	
-	# Apply gravity to velocity
+
+	# Dash cooldown timer
+	if dash_cd_left > 0.0:
+		dash_cd_left = max(0.0, dash_cd_left - delta)
+
+	# Apply gravity to velocity (keep gravity even during dash, usually feels better)
 	if has_gravity:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
@@ -95,14 +115,39 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed(input_jump) and is_on_floor():
 			velocity.y = jump_velocity
 
-	# Modify speed based on sprinting
-	if can_sprint and Input.is_action_pressed(input_sprint):
-			move_speed = sprint_speed
+	# Start dash
+	if can_dash and (not dashing) and dash_cd_left <= 0.0 and Input.is_action_just_pressed(input_dash):
+		if (not dash_only_on_floor) or is_on_floor():
+			var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
+			var wish_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+			# If no input direction, dash forward
+			if wish_dir == Vector3.ZERO:
+				wish_dir = -transform.basis.z
+				wish_dir.y = 0
+				wish_dir = wish_dir.normalized()
+
+			dashing = true
+			dash_time_left = dash_duration
+			dash_cd_left = dash_cooldown
+			dash_dir = wish_dir
+
+	# If currently dashing, override horizontal velocity for dash duration
+	if dashing:
+		dash_time_left -= delta
+		velocity.x = dash_dir.x * dash_speed
+		velocity.z = dash_dir.z * dash_speed
+		if dash_time_left <= 0.0:
+			dashing = false
+
+	# Modify speed based on sprinting (only when not dashing)
+	if can_sprint and Input.is_action_pressed(input_sprint) and not dashing:
+		move_speed = sprint_speed
 	else:
 		move_speed = base_speed
 
-	# Apply desired movement to velocity
-	if can_move:
+	# Apply desired movement to velocity (skip while dashing so dash stays dominant)
+	if can_move and not dashing:
 		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
 		var move_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if move_dir:
@@ -111,13 +156,13 @@ func _physics_process(delta: float) -> void:
 		else:
 			velocity.x = move_toward(velocity.x, 0, move_speed)
 			velocity.z = move_toward(velocity.z, 0, move_speed)
-	else:
+	elif not can_move:
 		velocity.x = 0
 		velocity.y = 0
-	
+		velocity.z = 0
+
 	# Use velocity to actually move
 	move_and_slide()
-
 
 ## Rotate us to look around.
 ## Base of controller rotates around y (left/right). Head rotates around x (up/down).
@@ -131,7 +176,6 @@ func rotate_look(rot_input : Vector2):
 	head.transform.basis = Basis()
 	head.rotate_x(look_rotation.x)
 
-
 func enable_freefly():
 	collider.disabled = true
 	freeflying = true
@@ -141,16 +185,13 @@ func disable_freefly():
 	collider.disabled = false
 	freeflying = false
 
-
 func capture_mouse():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	mouse_captured = true
 
-
 func release_mouse():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	mouse_captured = false
-
 
 ## Checks if some Input Actions haven't been created.
 ## Disables functionality accordingly.
@@ -173,6 +214,9 @@ func check_input_mappings():
 	if can_sprint and not InputMap.has_action(input_sprint):
 		push_error("Sprinting disabled. No InputAction found for input_sprint: " + input_sprint)
 		can_sprint = false
+	if can_dash and not InputMap.has_action(input_dash):
+		push_error("Dash disabled. No InputAction found for input_dash: " + input_dash)
+		can_dash = false
 	if can_freefly and not InputMap.has_action(input_freefly):
 		push_error("Freefly disabled. No InputAction found for input_freefly: " + input_freefly)
 		can_freefly = false
