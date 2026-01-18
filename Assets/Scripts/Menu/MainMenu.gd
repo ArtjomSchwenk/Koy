5ca0d9e9506dc5b076254cc660faca4ea227db0c
DieserMastro <sinan_haddad@outlook.de>
2026-01-18 15:02:19 +0100

    lol

--- Assets/Scripts/Menu/MainMenu.gd
+++ Assets/Scripts/Menu/MainMenu.gd
@@ -4,7 +4,7 @@ extends CanvasLayer
 @onready var play_Button: Button =  $Menu/MarginContainer/VBoxContainer/PlayButton;
 @onready var settings_Button: Button = $Menu/MarginContainer/VBoxContainer/SettingsButton;
 @onready var quit_Button: Button= $Menu/MarginContainer/VBoxContainer/QuitButton;
-@onready var camera_Node: Camera3D = $Background/SubViewportContainer/SubViewport/Camera3D;
+@onready var cameraGimbal_Node: Node3D = $Background/SubViewportContainer/SubViewport/CameraGimbal;
 @onready var background3D_Node: Node3D = $Background/gebeude_1;
 
 @export var radius: float = 5.0;
@@ -12,13 +12,14 @@ extends CanvasLayer
 @export var speed: float = 5.0;
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	cameraCircularMotion()
 	
 	
 func cameraCircularMotion():
 	var x_pos: float = cos(angle);
 	var y_pos: float = sin(angle);
 
-	camera_Node.position.y = radius * y_pos;
+	cameraGimbal_Node.position.y = radius * y_pos;
+	cameraGimbal_Node.position.x = radius * x_pos;
 	
 	
