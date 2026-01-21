1c2e0e0b2d141017e5804ce4e36131b6a586de10
DieserMastro <sinan_haddad@outlook.de>
2026-01-21 22:17:13 +0100

    Loading Game now Smooth + doesnt crash game :D

--- Assets/Scripts/Game/game_manager.gd
+++ Assets/Scripts/Game/game_manager.gd
@@ -1,27 +1,29 @@
 extends Node
 
 const MAIN_MENU = preload("res://Assets/Scenes/Menus/MainMenu.tscn")
-const LOAD_SCREEN = preload("res://Assets/Scenes/Game/loading_screen.tscn")
+##const LOAD_SCREEN = preload("res://Assets/Scenes/Game/loading_screen.tscn") ## Not Needed for current scope of game :D
 const GAME_WORLD: NodePath = "res://Assets/Scenes/terrain_data/welt.tscn"
 
 var isLoading: bool;
 var load_progress = [];
-var load_Status = 0;
-enum GAME_STATE {START = 0, PLAY = 1, LOAD = 2, PAUSE = 3, CONTINUE = 4, QUIT = 5};
+var load_Status: int = 0;
+signal loadingDone
+
+enum GAME_STATE {QUIT = -1, START = 0, PLAY = 1, LOAD = 2, PAUSE = 3, CONTINUE = 4};
 static var currentGameState;
 static var current_Scene = null
 
 func _ready() -> void:
 	setGameState(GAME_STATE.START);
+	ResourceLoader.load_threaded_request(GAME_WORLD)
 	
-	
+
 func _process(delta: float) -> void:
 	if isLoading:
 		load_Status = ResourceLoader.load_threaded_get_status(GAME_WORLD, load_progress);
-		if load_Status == 1.0:
+		if load_Status == ResourceLoader.THREAD_LOAD_LOADED:
 			isLoading = false;
-			
-	
+			loadingDone.emit();
 	
 func goto_scene(scene_Resource):
 	call_deferred("_deferred_goto_scene", scene_Resource)
@@ -54,16 +56,18 @@ func changeGameState():
 			quitGame();
 			
 func loadMainMenu():
-	
 	isLoading = true;
-	ResourceLoader.load_threaded_request(GAME_WORLD);
+
 	goto_scene(MAIN_MENU)
 	
 func loadGame():
-	var game: PackedScene = load(GAME_WORLD)
+	if isLoading:
+		print("Waiting");
+		await loadingDone;
+	var game: PackedScene = ResourceLoader.load_threaded_get(GAME_WORLD);
 	goto_scene(game);
 	
-func loadLoadingScreen():
+func loadLoadingScreen(): ##Not necessary for current scope tbh
 	pass
 
 func pauseGame():
