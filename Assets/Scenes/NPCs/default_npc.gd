0712672bb87f7dffc111ce1062fb5a35d955f65e
Arti-S <contact@arti-s.com>
2026-01-26 14:39:35 +0100

    dialogue is working now

--- Assets/Scenes/NPCs/default_npc.gd
+++ Assets/Scenes/NPCs/default_npc.gd
@@ -1,8 +1,8 @@
 extends Interactable
 
-
+@export var dialogue_resource: DialogueResource 
 func initiateDialogue():
-	print("Dialogue Initiated :D");
+	DialogueManager.show_dialogue_balloon(dialogue_resource, "start")
 
 func _on_trigger_interaction() -> void:
 	initiateDialogue();
